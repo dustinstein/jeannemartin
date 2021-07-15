912a5c88f053931c0445657172b9c12e2b27adfa
Dustin Stein <dustin.s@gp.local>
2021-07-15 15:52:14 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require("path");
+//const enforce = require('express-sslify');
+const port = process.env.PORT || 8080;
+const app = express();
+
+//app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, function() {
+    console.log('Server listening on port '+ port);
+});
+
+console.log('Server started!');
